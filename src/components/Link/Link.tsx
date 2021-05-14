a9b0165ee31d96675b82bde02087b8dcec21130d
Luke_Z <zurawskiu@gmail.com>
2021-05-14 16:06:31 +0200

    routing work

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -3,13 +3,25 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 type LinkProps = {
-  children: React.ReactNode;
+  children: string;
   navTo: string;
 };
 
 const Link: React.FC<LinkProps> = ({ navTo, children }) => {
   return (
-    <NavLink to={navTo} exact sx={{ variant: 'text.nav' , padding: '10px', textAlign:'center'}}>
+    <NavLink
+      to={navTo}
+      exact
+      sx={{
+        variant: 'text.nav',
+        padding: '10px',
+        textAlign: 'center',
+        transition: '.3s linear',
+        '&:hover': {
+          color: 'secondary'
+        }
+      }}
+    >
       {children}
     </NavLink>
   );
--- src/view/App.tsx
+++ src/view/App.tsx
@@ -2,14 +2,30 @@ import React from 'react';
 import { ThemeProvider } from 'theme-ui';
 import theme from '../theme/toDoAppTheme';
 import Navigation from '../components/Navigation/Navigation';
-
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     // @ts-ignore
     <ThemeProvider theme={theme}>
       <Navigation>
-        <div>jakiś tekst</div>
+        <Switch>
+          <Route path="/homepage" exact>
+            <p>STRONA DOMOWA Z LOGOWANIEM</p>
+          </Route>
+          <Route path="/" exact>
+            <p>Dom</p>
+          </Route>
+          <Route path="/new-task" exact>
+            <p>Nowe zadanie</p>
+          </Route>
+          <Route path="/task-list" exact>
+            <p>Lista zadań</p>
+          </Route>
+          <Route path="/task-summary" exact>
+            <p>Statystyki</p>
+          </Route>
+        </Switch>
       </Navigation>
     </ThemeProvider>
   );
